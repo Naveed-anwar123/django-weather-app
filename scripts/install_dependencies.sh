9dcfa2b10bb7d5f8243565582315dde20e2172bb
naveed-anwar123 <naveedanwar152@gmail.com>
2025-06-28 22:51:32 +0500

    bug fix

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,11 +2,11 @@
 cd /home/ec2-user/django-weather-app
 
 # Ensure Python and pip are installed
-sudo amazon-linux-extras install python3.8 -y
-sudo yum install -y python3 python3-pip
+sudo amazon-linux-extras install nginx python3.8 -y
+sudo yum install -y python3.8 python3-pip
 
 # Create virtual environment inside the app folder
-python3 -m venv venv
+python3.8 -m venv venv
 
 # Activate it
 source venv/bin/activate
