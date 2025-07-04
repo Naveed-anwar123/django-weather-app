9d64afebcaebfd6448ea39825dd9c1bcefd7d60b
naveed-anwar123 <naveedanwar152@gmail.com>
2025-07-04 11:01:58 +0500

    update install dependencies

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,7 @@ sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl restart gunicorn
 sudo systemctl enable gunicorn
-sudo amazon-linux-extras install nginx1 -y
+#sudo amazon-linux-extras install nginx1 -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo systemctl restart nginx
